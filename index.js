df76fb9a26f2379491546afe3447470b59097dc4
DalitsoKasonde <dkasonde11@gmail.com>
2021-05-06 01:53:56 +0200

    manipulatin

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title margin-50");
+
+// Tap into css
+// $("h1").css("color", "red");
+
+// get property of Element
+// console.log($("h1").css("color"));
+// console.log($("h1").css("font-size"));
 
 // To use when script is not placed at the end
 // $(document).ready(function() {
